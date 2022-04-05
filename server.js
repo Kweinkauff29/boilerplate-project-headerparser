5320286f5295bba7bb1302ff0da59e71be0f6106
Kweinkauff29 <79805109+Kweinkauff29@users.noreply.github.com>
2022-04-04 20:20:46 -0400

    initial push

--- server.js
+++ server.js
@@ -26,9 +26,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  console.log("test");
+  res.json("test");
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.Port, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
